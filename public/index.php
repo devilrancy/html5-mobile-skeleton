b9900b835d1477b819ff4ecb9c26d67652e3c1e2
Christian Seidl <seidl@elbdudler.de>
2013-02-11 23:14:25 +0100

    Slim app.

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Slim,
+    Slim\Extras\Views\Twig as Twig;
+
+Slim::registerAutoloader();
+$app = new Slim(array(
+    'view' => new Twig,
+    'templates.path' => __DIR__ . '/../templates/',
+));
+
+
+$app->get('/', function() use ($app) {
+
+  $greetingsEarthlings = 'Hi, how are you?'
+  $app->render(
+    'index.twig',
+    array(
+      'greetingsEarthlings' => $greetingsEarthlings
+    )
+  );
+});
+
+
+$app->run();
